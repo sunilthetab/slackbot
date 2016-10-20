9d50e21520463ff2b39e1048009b78c28466f519
Sohan Kunkerkar <sakunker@ncsu.edu>
2016-10-20 00:03:54 -0400

    Update SlackTest.java

--- Milestone 2/Selenium_Test/SlackTest.java
+++ Milestone 2/Selenium_Test/SlackTest.java
@@ -1,11 +1,5 @@
 package selenium.tests;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,7 +15,8 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class SlackTest {
 private static WebDriver driver;
-	
+WebDriverWait wait = new WebDriverWait(driver, 30);
+
 	@BeforeClass
 	public static void setUp() throws Exception 
 	{
@@ -36,9 +31,10 @@ private static WebDriver driver;
 		driver.close();
 		driver.quit();
 	}
+
 	
 	@Test
-	public void postMessage()
+	public void postMessage() 
 	{
 		driver.get("https://seprojbot.slack.com/");
 
@@ -64,40 +60,41 @@ private static WebDriver driver;
 		// Switch to #bots channel and wait for it to load.
 		driver.get("https://seprojbot.slack.com/messages/azra_testing");
 		wait.until(ExpectedConditions.titleContains("azra_testing"));
-
-		// Type something
+		// Type something 
 		WebElement messageBot = driver.findElement(By.id("message-input"));
 		messageBot.sendKeys("@azra setup");
 		messageBot.sendKeys(Keys.RETURN);
-
-		//wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
-
 		WebElement msg = driver.findElement(
-				By.xpath("//span[@class='message_body' and text() = ' Let's organize a new meeting']"));
+				By.xpath("//span[contains(@class,'message_body') and text() = 'Alright. May I know the email IDs of the attendees, please?']"));
+		
 		assertNotNull(msg);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Alright. May I know the email IDs of the attendees, please?']")));
+		WebElement messageBot1 = driver.findElement(By.id("message-input"));
+		messageBot1.sendKeys("@azra pranav,sohan,gautam,sunil,ajay");
 
-
-
-		WebElement msg2 = driver.findElement(By.xpath("//span[@class='message_body' and text() = 'OK. What will be the approximate duration of the meeting (HH:MM or HH)?']"));
+		messageBot1.sendKeys(Keys.RETURN);
+		WebElement msg1 = driver.findElement(
+				By.xpath("//span[contains(@class,'message_body') and text() = 'OK. What will be the approximate duration of the meeting (HH:MM or HH)?']"));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'OK. What will be the approximate duration of the meeting (HH:MM or HH)?']")));
+		assertNotNull(msg1);
+		
+		WebElement messageBot2 = driver.findElement(By.id("message-input"));
+		messageBot2.sendKeys("@azra 2");
+		messageBot2.sendKeys(Keys.RETURN);
+		
+		WebElement msg2 = driver.findElement(By.xpath("//span[@class='message_body' and text() = 'And by what date(MM/DD/YYYY or MM/DD or DD) or day do you want the meeting to be scheduled? Say NA if no such constraint']"));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'And by what date(MM/DD/YYYY or MM/DD or DD) or day do you want the meeting to be scheduled? Say NA if no such constraint']")));
 		assertNotNull(msg2);
-		
-		messageBot.sendKeys("2");
-		messageBot.sendKeys(Keys.RETURN);
-		
-		WebElement msg3 = driver.findElement(By.xpath("//span[@class='message_body' and text() = 'And by what date(MM/DD/YYYY or MM/DD or DD) or day do you want the meeting to be scheduled? Say NA if no such constraint']"));
-        assertNotNull(msg3);
-        
-        messageBot.sendKeys("10/26");
-		messageBot.sendKeys(Keys.RETURN);
-        
-        
-        
+		WebElement messageBot3 = driver.findElement(By.id("message-input"));
+		messageBot3.sendKeys("10/26");
+		messageBot3.sendKeys(Keys.RETURN);
 		WebElement msg4 = driver.findElement(By.xpath("//span[@class='message_body' and text() = 'OK. By what time (HH:MM or HH) should the meeting be organized (24 Hour format)? Say NA if no such constraint']"));
-        assertNotNull(msg4);
-        
-        messageBot.sendKeys("17");
-		messageBot.sendKeys(Keys.RETURN);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() ='OK. By what time (HH:MM or HH) should the meeting be organized (24 Hour format)? Say NA if no such constraint']")));
+		assertNotNull(msg4);
+		
+		}
+	
 
 	}
-}
+
 
