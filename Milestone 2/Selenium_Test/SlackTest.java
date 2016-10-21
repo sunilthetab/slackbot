64b3c445c63d243c00bff93c4ea0e2aa31d2a5c9
gverma <gverma@ncsu.edu>
2016-10-20 21:47:49 -0400

    Added necessary package name

--- Milestone 2/Selenium_Test/SlackTest.java
+++ Milestone 2/Selenium_Test/SlackTest.java
@@ -1,10 +1,14 @@
 package selenium.tests;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
