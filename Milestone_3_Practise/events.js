4b62cd471fd8a8ffc51efffbc9e118c799c31f93
Sunil Narasimhamurthy <snarasi5@ncsu.edu>
2016-11-09 19:04:48 -0500

    event.js add remove user in event

--- Milestone_3_Practise/events.js
+++ Milestone_3_Practise/events.js
@@ -1,14 +1,10 @@
-/**
- * Created by Sunil on 11/9/2016.
- */
-// var _ = require('underscore'),
 //     Util = require("util"),
 //     helpers = require('../lib/helpers'),
 //     googleapis = require('googleapis'),
 //     //CALLBACK_URL= process.env.HUBOT_URL + "/google/calendar/webhook",
 //     uuid = require('node-uuid'),
 //     moment = require("moment");
-
+var _ = require('underscore'),
 
 var events = {
 
@@ -33,21 +29,91 @@ var events = {
         });
     },
 
-    event_patch: function (calendarID) {
-        // var range = moment.parseZone(event.start.dateTime || event.start.date)
-        //     .twix(moment.parseZone(event.end.dateTime || event.end.date));
-        // return range.format();
-        /*calendar.events.insert({
-            auth: auth,
-            calendarId: 'primary',
-            resource: event,
-        }, function (err, event) {
+    event_patch_add_users: function (calendar, auth, eventID, emails_list) {
+
+        //TODO: add logic to extract emailid if only username or name of users are provided.
+        // now we are expecting user to input in format "apendya@ncsu.edu ppfirake@ncsu.edu"
+        var emails = _.compact(_.map(_.compact(emails_list.split(' '))));
+
+        calendar.events.get({ auth: auth,
+            alwaysIncludeEmail: true,
+            calendarId: "primary",
+            eventId: eventID
+        }, function(err, event) {
             if (err) {
-                console.log('There was an error contacting the Calendar service: ' + err);
+                console.log('Error getting event: ' + err);
                 return;
             }
-            console.log('Event created: %s', event.htmlLink);
-        });*/
+            var emailsObj = _.map(emails, function (a) {
+                return {email: a};
+            });
+
+            var current_emails = _.map(event.attendees, function (a) {
+                return {email: a.email};
+            });
+            var emailUnion = _.union(emailsObj, current_emails);
+
+            calendar.events.patch({
+                auth: auth,
+                calendarId: "primary",
+                eventId: event.id,
+                resource: {attendees: emailUnion}
+            }, function (err, event) {
+                if (err) {
+                    console.log('Error inviting additional users: ' + err);
+                    return;
+                }
+                console.log("Users invitation success for Event with ID: %s", event.id);
+            });
+        });
+    },
+
+    event_patch_remove_users: function (calendar, auth, eventID, emails_list) {
+        //TODO: add logic to extract emailid if only username or name of users are provided.
+        // now we are expecting user to input in format "apendya@ncsu.edu ppfirake@ncsu.edu"
+        var emails = _.compact(_.map(_.compact(emails_list.split(' '))));
+        calendar.events.get({ auth: auth,
+            alwaysIncludeEmail: true,
+            calendarId: "primary",
+            eventId: eventID
+        }, function(err, event) {
+            if (err) {
+                console.log('Error getting event: ' + err);
+                return;
+            }
+            var emailsObj = _.map(emails, function (a) {
+                return {email: a};
+            });
+
+            var current_emails = _.map(event.attendees, function (a) {
+                return {email: a.email};
+            });
+
+            var emailDisjunction = [];
+            for (var i = 0; i < current_emails.length; i++)
+            {
+                var isPresent = _.find(emailsObj, function(d){
+                                 return d.email === current_emails[i].email;
+                        });
+                if(!isPresent)
+                {
+                    emailDisjunction.push(current_emails[i]);
+                }
+            }
+
+            calendar.events.patch({
+                auth: auth,
+                calendarId: "primary",
+                eventId: event.id,
+                resource: {attendees: emailDisjunction}
+            }, function (err, event) {
+                if (err) {
+                    console.log('Error inviting additional users: ' + err);
+                    return;
+                }
+                console.log("Users invitation success for Event with ID: %s", event.id);
+            });
+        });
     },
 
     event_delete: function (calendar,auth, eventID) {
